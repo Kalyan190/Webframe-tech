00f294c13b5cee340e02b20fb5d5ceb8d0d3b228
Kalyan Kumar <kalyaniit795@gmail.com>
2024-12-09 21:15:13 +0530

    error occur navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import ArticleSimilar from "@/components/ArticleSimilar";
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import Product from "@/components/Product";
 import ThreeProduct from "@/components/ThreeProduct";
 
 export default function Home() {
   return (
     <div >
-      {/* <Navbar/> */}
+      
       <Product/>
       <ArticleSimilar/>
       <ThreeProduct/>
